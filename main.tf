bfaa43aabab59ae5c6b983d695b11c1bc1e9f70b
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-04 16:01:27 +0200

    file finished

--- main.tf
+++ main.tf
@@ -4,50 +4,20 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  aks_name = "myAKSCluster"
-}
-
 # Utilisation du module réseau
-module "network" {
-  source = "git::https://github.com/<votre-nom-d-utilisateur>/<nom-du-repo-du-module>.git"
+module "deployment" {
+  source = "../Brief_12Bis"
   
+  aks_name            = "db12-AKS"
   resource_group_name = "myResourceGroup"
   location            = "eastus"
   vnet_name           = "myVNet"
   address_space       = "10.0.0.0/16"
   subnet_name         = "mySubnet"
   subnet_prefix       = "10.0.1.0/24"
-  # ajoutez tous les autres paramètres que vous souhaitez personnaliser
-}
-
-# Déploiement du cluster AKS
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = local.aks_name
-  location            = module.network.location
-  resource_group_name = module.network.resource_group_name
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-    vnet_subnet_id = module.network.subnet_id
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
-# Exposer certaines informations sur le cluster
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  node_count          = "2"
+  vm_size             = "Standard_D2_v2"
+  vnet_subnet_id      = module.deployment.subnet_id
+  identity            = "SystemAssigned"
+  tags                = "dev"
 }
