e6129b24d531d0eef67469a774ea4d830d5bd1f4
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 14:11:57 +0200

    [ ] added

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ module "deployment" {
   resource_group_name = "db12-rg"
   location            = "francecentral"
   vnet_name           = "db12-vnet"
-  address_space       = "10.0.0.0/16"
+  address_space       = ["10.0.0.0/16"]
   subnet1_name        = "db12-sbnt"
-  subnet1_prefix      = "10.0.1.0/16"
+  subnet1_prefix      = ["10.0.1.0/16"]
   gateway_name        = "db12_gateway"
   /* sku_name            = "Static" */
   pubIP_gateway_name  = "db12_gateway_pubIP"
@@ -21,9 +21,9 @@ module "deployment" {
   pubIP_sku           = "Standard"
   routeTab_name       = "db12_routeTab"
   priv_subnet_name    = "db12_priv_sbnt"
-  priv_sbnt_add_pref  = "10.0.2.0/16"
+  priv_sbnt_add_pref  = ["10.0.2.0/16"]
   pub_subnet_name     = "db12_pub_sbnt"
-  pub_sbnt_add_pref   = "10.0.3.0/16"
+  pub_sbnt_add_pref   = ["10.0.3.0/16"]
   aks_name            = "db12-AKS"
   node_count          = 2
   vm_size             = "Standard_D2_v2"
