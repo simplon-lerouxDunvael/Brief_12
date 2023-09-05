39e1da687161750f65b72af26a62653644b4ad93
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 13:46:44 +0200

    path updated

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 
 # Utilisation du module
 module "deployment" {
-  source = "git::https://github.com/simplon-lerouxDunvael/Brief_12Bis" # https://github.com/simplon-lerouxDunvael/Brief_12Bis / https://github.com/simplon-lerouxDunvael/Brief_12Ter
+  source = "git::https://github.com/simplon-lerouxDunvael/Brief_12Ter" # https://github.com/simplon-lerouxDunvael/Brief_12Bis / https://github.com/simplon-lerouxDunvael/Brief_12Ter
   
   resource_group_name = "db12-rg"
   location            = "francecentral"
