7911f5780939ef7eca60417186c8e4df302272cc
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 11:27:09 +0200

    vars added to deployment file

--- main.tf
+++ main.tf
@@ -8,16 +8,26 @@ provider "azurerm" {
 module "deployment" {
   source = "git::https://github.com/simplon-lerouxDunvael/Brief_12bis"
   
-  aks_name            = "db12-AKS"
   resource_group_name = "myResourceGroup"
   location            = "eastus"
   vnet_name           = "myVNet"
   address_space       = "10.0.0.0/16"
   subnet_name         = "mySubnet"
   subnet_prefix       = "10.0.1.0/24"
+  gateway_name        = "db12_gateway"
+  pubIP_gateway_name  = "db12_gateway_pubIP"
+  pubIP_allocation    = "Static"
+  pubIP_sku           = "Standard"
+  routeTab_name       = "Standard"
+  priv_subnet_name    = "db12_priv_sbnt"
+  priv_sbnt_add_pref  = "10.0.2.0/16"
+  pub_subnet_name     = "db12_pub_sbnt"
+  pub_sbnt_add_pref   = "10.0.3.0/16"
+  aks_name            = "db12-AKS"
   node_count          = "2"
   vm_size             = "Standard_D2_v2"
   vnet_subnet_id      = module.deployment.subnet_id
   identity            = "SystemAssigned"
   tags                = "dev"
+
 }
