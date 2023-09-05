3c36e0f0bc8287fb8f5040b9c8042eec283348f4
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-09-05 14:52:11 +0200

    add cidr updated

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "deployment" {
   priv_subnet_name    = "db12_priv_sbnt"
   priv_sbnt_add_pref  = ["10.0.2.0/24"]
   pub_subnet_name     = "db12_pub_sbnt"
-  pub_sbnt_add_pref   = ["10.0.3.0/24"]
+  pub_sbnt_add_pref   = ["10.0.6.0/24"]
   aks_name            = "db12-AKS"
   dns_prefix          = "aks-db12"
   node_pool_name      = "db12pool"
